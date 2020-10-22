66d76e84b232bd03f1248505a345881a5e5fc1c7
Bret Baker <bretbaker808@gmail.com>
2020-10-22 08:27:24 -0400

    nc

---
+++ HRJava/src/practice/easy/Anagrams.java
@@ -0,0 +1,61 @@
+package practice.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Anagrams {
+
+	public static void main(String[] args) {
+		isAnagram("Thomas", "brAdy");
+	}
+
+	static boolean isAnagram(String a, String b) {
+
+		// test for invalid input   
+		if( a == null || b == null || a.equals("") || b.equals("") )
+		    throw new IllegalArgumentException();
+
+		// initial quick test for non-anagrams
+		if ( a.length() != b.length() )
+		    return false;
+
+		a = a.toLowerCase();
+		b = b.toLowerCase();
+
+		// populate a map with letters and frequencies of String b
+		Map<Character, Integer> map = new HashMap<>();
+
+		for (int k = 0; k < b.length(); k++){
+		     char letter = b.charAt(k);    
+
+		    if( ! map.containsKey(letter)){
+		        map.put( letter, 1 );
+		    } else {
+		        Integer frequency = map.get( letter );
+		        map.put( letter, ++frequency );
+		    }
+		}
+
+		// test each letter in String a against data in the map
+		// return if letter is absent in the map or its  frequency is 0
+		// otherwise decrease the frequency by 1
+
+		for (int k = 0; k < a.length(); k++){
+		    char letter = a.charAt(k);
+
+		    if( ! map.containsKey( letter ) )
+		        return false;
+
+		    Integer frequency = map.get( letter );
+
+		     if( frequency == 0 )
+		        return false;
+		    else    
+		        map.put( letter, --frequency);
+		}
+		// if the code got that far it is an anagram
+		return true;
+
+	}
+	
+}
