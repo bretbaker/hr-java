df7ae9ccf12a8911b22e7ad902634ebb782ba7f7
Bret Baker <bretbaker808@gmail.com>
2020-11-02 07:35:49 -0500

    nc

---
+++ HRJava/src/practice/easy/BigIntegerC.java
@@ -0,0 +1,15 @@
+package practice.easy;
+
+import java.math.BigInteger;
+
+public class BigIntegerC {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BigInteger bi1 = new BigInteger("1234");
+		BigInteger bi2 = new BigInteger("20");
+		System.out.println(bi1.add(bi2));
+		System.out.println(bi1.multiply(bi2));
+	}
+
+}
