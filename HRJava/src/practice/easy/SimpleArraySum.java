3a99c43a070973353c5ab5dfcb1495e5512ac306
Bret Baker <bretbaker808@gmail.com>
2022-09-07 15:17:09 -0400

    add SimpleArraySum challenge completed

--- HRJava/src/practice/easy/SimpleArraySum.java
+++ HRJava/src/practice/easy/SimpleArraySum.java
@@ -1,10 +1,24 @@
 package practice.easy;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class SimpleArraySum {
+	
+	public static ArrayList<Integer> arr;
+	public static Integer sum;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		arr = new ArrayList<Integer>(Arrays.asList(1,2,3,4,10,11));
+		System.out.println(simpleArraySum(arr));
 	}
+	
+    public static Integer simpleArraySum(ArrayList<Integer> ar) {
+		sum = new Integer(0);
+    	for (Integer i : ar) {
+			sum+=i;
+		}
+    	return sum;
+    }
 
 }
