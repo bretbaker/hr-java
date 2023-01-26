76a8d8f5b470d1987f5ee8a0be45cb3762e8f67b
Bret Baker <bretbaker808@gmail.com>
2023-01-26 09:55:05 -0500

    add algorithm and step through of code via commentary

--- HRJava/src/practice/medium/JavaStack.java
+++ HRJava/src/practice/medium/JavaStack.java
@@ -5,38 +5,62 @@ import java.util.Stack;
 
 public class JavaStack {
 
+	/*
+	 * algorithm 
+	 * 
+	 * (this algorithm assumes that the parentheses will always start with
+	 * an open bracket and will either balance with a closing bracket or remain
+	 * unbalanced with no closing bracket) 
+	 * 
+	 * 1) iterate over each ArrayList one by one
+	 * 2) read each element while iterating
+	 * 3) we need to utilize two different data structures simultaneously, ArrayList for reading, and Stack for writing
+	 * 4) if current element is an opening bracket we push it to stack
+	 * 5) if current element is a closing bracket we read the last element pushed to stack, and if assumption holds true,
+	 *    it should be a matching pair, which then we would remove the opening bracket from the stack, instead of pushing anything to stack
+	 * 6) once done iterating, if this assumption holds true, this algorithm should return an empty stack if balanced
+	 * 
+	 */
+
 	static ArrayList<String> arr0;
 	static ArrayList<String> arr1;
-//	static ArrayList<String> arr2;
 	static Stack<String> st0;
 	static int index = 0;
-//	static boolean balanced = false;
 
 	public static void main(String[] args) {
 
+//		this is our ArrayList holding the bracket pairs to be iterated over
 		arr0 = new ArrayList<String>();
 		arr0.add("{}()");
 		arr0.add("({()})");
 		arr0.add("{}(");
 		arr0.add("[]");
+		
+//		this is for visual purposes
 		System.out.println(arr0.toString());
+		
+//		here is our actual invocation of our solution implementation
 		System.out.println(findBalance(arr0).toString());
 
 	}
 
 	static ArrayList<String> findBalance(ArrayList<String> ar0) {
 
+//		convert each bracket pair group to an ArrayList of Strings for easy iteration/reading
 		arr1 = new ArrayList<String>();
-//		arr2 = new ArrayList<String>();
+		
+//		instantiate our Stack data structure
 		st0 = new Stack<String>();
+		
+//		iterate over the ArrayLists of Strings and implement our algorithm
 		for (int i = 0; i < ar0.size(); i++) {
 
-//			arr1.clear();
 			st0.clear();
 
 			for (int j = 0; j < ar0.get(i).length(); j++) {
+
 				String c = Character.toString(ar0.get(i).charAt(j));
-//				arr1.add(c);
+
 				if (c.equals("(") || c.equals("{") || c.equals("[")) {
 					st0.push(c);
 				} else if ((c.equals(")") && st0.peek().equals("(")) || (c.equals("}") && st0.peek().equals("{"))
@@ -52,7 +76,6 @@ public class JavaStack {
 			} else {
 				arr1.add("false");
 			}
-//			System.out.println(arr1.toString());
 
 		}
 
