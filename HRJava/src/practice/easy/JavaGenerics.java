e22b7a5a1479318c9d46b2f5d1034501ba824704
Bret Baker <bretbaker808@gmail.com>
2023-02-06 11:20:39 -0500

    add completed JavaGenerics challenge

--- HRJava/src/practice/easy/JavaGenerics.java
+++ HRJava/src/practice/easy/JavaGenerics.java
@@ -1,10 +1,22 @@
 package practice.easy;
 
 public class JavaGenerics {
+	
+	/*
+	 * it is very important to note that generics do not work with primitives
+	 * if you want an generic method to work on a primitive, you need to use an object representation of that data
+	 * aka a wrapper class as we use for the integer array below
+	 */
+	
+	static Integer[] intArr;
+	static String[] stringArr;
 
 	public static void main(String[] args) {
 		
-		
+		intArr = new Integer[] {1,2,3};
+		stringArr = new String[] {"Hello","World"};
+		printArray(intArr);
+		printArray(stringArr);
 
 	}
 	
